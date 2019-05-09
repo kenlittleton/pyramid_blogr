383b2c7398fdcc04b78af51cc4ac7e952f1ad27f
Ken Littleton <ken.littleton@gmail.com>
2019-05-09 16:47:46 -0500

    Added delele post functionality (Issue #6)

--- pyramid_blogr/security.py
+++ pyramid_blogr/security.py
@@ -4,7 +4,8 @@ from pyramid.security import Allow, Everyone, Authenticated
 class BlogRecordFactory(object):
     __acl__ = [(Allow, Everyone, 'view'),
                (Allow, Authenticated, 'create'),
-               (Allow, Authenticated, 'edit'), ]
+               (Allow, Authenticated, 'edit'),
+               (Allow, Authenticated, "delete"), ]
 
     def __init__(self, request):
         pass
